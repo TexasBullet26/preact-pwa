e432d3f09b751b41e0ede12bad87b7c62493fde9
Glenn Lanzer <lanzertrey@gmail.com>
2018-10-10 07:09:36 -0500

    Add: components About, App, Articles, Nav

---
+++ src/app/components/About.js
@@ -0,0 +1,9 @@
+import {h, Component} from 'preact' // eslint-disable-line no-unused-vars
+export default() => (
+    <div className='content About'>
+        <p>Super fast progressive web app</p>
+        <p>Github:
+            <a href='https://github.com/TexasBullet26'>github.com/TexasBullet26</a>
+        </p>
+    </div>
+)
