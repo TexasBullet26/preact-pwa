346fc5e0e933334c7dd4541007749b2b1220814f
Glenn Lanzer <lanzertrey@gmail.com>
2018-10-11 04:20:31 -0500

    Add: Posts component

---
+++ src/app/components/Posts.js
@@ -0,0 +1,26 @@
+import {h} from 'preact'
+import PreactRedux from 'preact-redux'
+import {getPosts} from './../store/selectors/posts'
+import {updateLocation} from './../store/actions/meta'
+import Link from './Link'
+const {connect} = PreactRedux
+
+const Post = ({post, _updateLocation}) => (
+    <li className='Post'>
+        <Link
+            href={`/blog/${post.id}`}
+            onClick={() => _updateLocation(`/blog/${post.id}`)}>
+            {post.title}
+        </Link>
+    </li>
+)
+
+const Posts = ({posts, _updateLocation}) => (
+    <ul className='Posts page'>
+        {posts.map((post, i) => (<Post _updateLocation={_updateLocation} post={post} key={i}/>))}
+    </ul>
+)
+
+export default connect((state) => ({posts: getPosts(state)}), (dispatch) => ({
+    _updateLocation: (url) => dispatch(_updateLocation(url))
+}))(Posts)
