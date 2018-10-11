20e3db7337cae2d3a39daddf1671b92aa0b0ca18
Glenn Lanzer <lanzertrey@gmail.com>
2018-10-11 04:18:16 -0500

    Edit: About component

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -1,7 +1,8 @@
 import {h, Component} from 'preact' // eslint-disable-line no-unused-vars
 export default() => (
-    <div className='content About'>
-        <p>Super fast progressive web app</p>
+    <div className='About page'>
+        <h2>About</h2>
+        <strong>TexasBullet26</strong>
         <p>Github:
             <a href='https://github.com/TexasBullet26'>github.com/TexasBullet26</a>
         </p>
